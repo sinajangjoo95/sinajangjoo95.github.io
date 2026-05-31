415e35f4192726ceb80652c0dea36e7505aa2786
sinajangjoo95 <sina.jangjoo@gmail.com>
2026-05-31 13:58:04 -0400

    Update blog-components.js

--- blog-components.js
+++ blog-components.js
@@ -7,7 +7,6 @@
 
   const areaMeta = {
     crf: {
-      published: false,
       label: "Contested Rule Formation and Institutional Mismatch",
       icon: "images/extracted_icon_2.png",
       order: 1
